de053bf12cdbf9b6d794f6426cf801a4ca769c32
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-12-31 16:53:44 -0600

    Extract notationToScore helper

--- bowling.go
+++ bowling.go
@@ -3,19 +3,23 @@ package bowling
 import "strconv"
 
 func Score(frames []string) int {
-	var score int = 0
+	score := 0
 	for _, frame := range frames {
 		if len(frame) == 2 {
 			frame = frame[0:1]
 		}
 		if len(frame) == 3 {
-			firstThrow, _ := strconv.Atoi(frame[0:1])
-			secondThrow, _ := strconv.Atoi(frame[2:3])
+			firstThrow := notationToScore(frame[0:1])
+			secondThrow := notationToScore(frame[2:3])
 			score += firstThrow + secondThrow
 			continue
 		}
-		frameScore, _ := strconv.Atoi(frame)
-		score += frameScore
+		score += notationToScore(frame)
 	}
 	return score
+}
+
+func notationToScore(value string) int {
+	convertedNumber, _ := strconv.Atoi(value)
+	return convertedNumber
 }
