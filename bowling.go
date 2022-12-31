912834aa27afd601aa315441e49962bdc3d5474e
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-12-31 16:36:33 -0600

    Interpret dash as miss

--- bowling.go
+++ bowling.go
@@ -5,6 +5,9 @@ import "strconv"
 func Score(frames []string) int {
 	var score int = 0
 	for _, frame := range frames {
+		if len(frame) == 2 {
+			frame = frame[0:1]
+		}
 		frameScore, _ := strconv.Atoi(frame)
 		score += frameScore
 	}
--- bowling_test.go
+++ bowling_test.go
@@ -14,3 +14,12 @@ func TestScore_ReturnsNumberOfKnockedDownPins(t *testing.T) {
 		t.Errorf("expected: '%v' actual: '%v", expected, actual)
 	}
 }
+
+func TestScore_DashIsAMiss(t *testing.T) {
+	const expected int = 3
+	line := []string{"1-", "2-"}
+	actual := bowling.Score(line)
+	if actual != expected {
+		t.Errorf("expected: '%v' actual: '%v", expected, actual)
+	}
+}
