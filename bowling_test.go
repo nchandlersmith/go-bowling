1f1b7cd7c09bb0110996c7d78a4c266718c2d449
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-12-31 19:42:54 -0600

    Table-driven tests yo!

--- bowling_test.go
+++ bowling_test.go
@@ -6,74 +6,57 @@ import (
 	"go-bowling"
 )
 
-func TestScore_ReturnsNumberOfKnockedDownPins(t *testing.T) {
-	line := []string{"1/6", "7/1"}
-	const expected = 15
-	actual := bowling.Score(line)
-	if actual != expected {
-		t.Errorf("expected: '%v' actual: '%v", expected, actual)
+func TestScore(t *testing.T) {
+	tests := []struct {
+		name string
+		line []string
+		want int
+	}{
+		{
+			name: "Returns number of knocked down pines",
+			line: []string{"1/6", "7/1"},
+			want: 15,
+		},
+		{
+			name: "Dash is a miss",
+			line: []string{"1-", "2-"},
+			want: 3,
+		},
+		{
+			name: "Score a spare",
+			line: []string{"8/", "7/1"},
+			want: 25,
+		},
+		{
+			name: "Score a strike",
+			line: []string{"x", "7/1"},
+			want: 25,
+		},
+		{
+			name: "Score a perfect game",
+			line: []string{"x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"},
+			want: 300,
+		},
+		{
+			name: "10 turns of 9 and a miss",
+			line: []string{"9-", "9-", "9-", "9-", "9-", "9-", "9-", "9-", "9-", "9-"},
+			want: 90,
+		},
+		{
+			name: "10 turns of 5 and spare plus 5",
+			line: []string{"5/", "5/", "5/", "5/", "5/", "5/", "5/", "5/", "5/", "5/", "5"},
+			want: 150,
+		},
+		{
+			name: "strike then spare",
+			line: []string{"x", "5/", "5/2"},
+			want: 37,
+		},
 	}
-}
-
-func TestScore_DashIsAMiss(t *testing.T) {
-	line := []string{"1-", "2-"}
-	const expected = 3
-	actual := bowling.Score(line)
-	if actual != expected {
-		t.Errorf("expected: '%v' actual: '%v", expected, actual)
-	}
-}
-
-func TestScore_ScoreSpare(t *testing.T) {
-	line := []string{"8/", "7/1"}
-	const expected = 25
-	actual := bowling.Score(line)
-	if actual != expected {
-		t.Errorf("expected: '%v' actual: '%v", expected, actual)
-	}
-}
-
-func TestScore_ScoreStrike(t *testing.T) {
-	line := []string{"x", "7/1"}
-	const expected = 25
-	actual := bowling.Score(line)
-	if actual != expected {
-		t.Errorf("expected: '%v' actual '%v'", expected, actual)
-	}
-}
-
-func TestScore_PerfectGame(t *testing.T) {
-	line := []string{"x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"}
-	const expected = 300
-	actual := bowling.Score(line)
-	if actual != expected {
-		t.Errorf("expected: '%v' actual '%v'", expected, actual)
-	}
-}
-
-func TestScore_10TurnsOf9AndMiss(t *testing.T) {
-	line := []string{"9-", "9-", "9-", "9-", "9-", "9-", "9-", "9-", "9-", "9-"}
-	const expected = 90
-	actual := bowling.Score(line)
-	if actual != expected {
-		t.Errorf("expected: '%v' actual '%v'", expected, actual)
-	}
-}
-
-func TestScore_10TurnsOf5AndSparePlus5(t *testing.T) {
-	line := []string{"5/", "5/", "5/", "5/", "5/", "5/", "5/", "5/", "5/", "5/", "5"}
-	const expected = 150
-	actual := bowling.Score(line)
-	if actual != expected {
-		t.Errorf("expected: '%v' actual '%v'", expected, actual)
-	}
-}
-
-func TestScore_StrikeThenSpare(t *testing.T) {
-	line := []string{"x", "5/", "5/2"}
-	const expected = 37
-	actual := bowling.Score(line)
-	if actual != expected {
-		t.Errorf("expected: '%v' actual '%v'", expected, actual)
+	for _, test := range tests {
+		got := bowling.Score(test.line)
+		if got != test.want {
+			t.Errorf("Test: %v. Got: %v Want: %v", test.name, got, test.want)
+		}
 	}
 }
