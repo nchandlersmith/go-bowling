6a90c4feb9d27be4f31c51ca002746c727ae1259
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2022-12-31 18:59:13 -0600

    Use switch case

--- bowling.go
+++ bowling.go
@@ -5,29 +5,49 @@ import "strconv"
 func Score(frames []string) int {
 	score := 0
 	for i, turn := range frames {
-		if i > 9 {
-			continue
+		switch true {
+		case isExtraTurn(i):
+			// no -op
+		case isStrike(turn):
+			score = scoreStrike(frames, i, score)
+		case isSpare(turn):
+			score = scoreSpare(frames, i, score)
+		default:
+			score += translate(turn)
 		}
-		if turn == "x" {
-			nextThrow := frames[i+1][0:1]
-			var throwAfterNext string
-			if len(frames[i+1]) == 1 {
-				throwAfterNext = frames[i+2][0:1]
-			} else {
-				throwAfterNext = frames[i+1][1:2]
-			}
-			scoreNextThrow := translate(nextThrow)
-			scoreThrowAfterNext := translate(throwAfterNext)
-			score += 10 + scoreNextThrow + scoreThrowAfterNext
-			continue
-		}
-		if turn[1:2] == "/" && len(turn) == 2 {
-			scoreNextThrow := translate(frames[i+1][0:1])
-			score += 10 + scoreNextThrow
-			continue
-		}
-		score += translate(turn)
 	}
+	return score
+}
+
+func isExtraTurn(i int) bool {
+	return i > 9
+}
+
+func isStrike(turn string) bool {
+	return turn == "x"
+}
+
+func scoreStrike(frames []string, i int, score int) int {
+	nextThrow := frames[i+1][0:1]
+	var throwAfterNext string
+	if len(frames[i+1]) == 1 {
+		throwAfterNext = frames[i+2][0:1]
+	} else {
+		throwAfterNext = frames[i+1][1:2]
+	}
+	scoreNextThrow := translate(nextThrow)
+	scoreThrowAfterNext := translate(throwAfterNext)
+	score += 10 + scoreNextThrow + scoreThrowAfterNext
+	return score
+}
+
+func isSpare(turn string) bool {
+	return turn[1:2] == "/" && len(turn) == 2
+}
+
+func scoreSpare(frames []string, i int, score int) int {
+	scoreNextThrow := translate(frames[i+1][0:1])
+	score += 10 + scoreNextThrow
 	return score
 }
 
